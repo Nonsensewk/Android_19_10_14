a8ffd5698b6782e915780384bf28ce640063b7bf
wk157 <1596386111@qq.com>
2019-10-14 20:13:59 +0800

    2019_10_14 20:13

--- app/src/main/java/ies/syuct/edu/cn/MainActivity.java
+++ app/src/main/java/ies/syuct/edu/cn/MainActivity.java
@@ -1,7 +1,9 @@
-package ies.syuct.edu.cn.ies;
+package ies.syuct.edu.cn;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ies.syuct.edu.cn.ies.R;
 
 public class MainActivity extends AppCompatActivity {
 
